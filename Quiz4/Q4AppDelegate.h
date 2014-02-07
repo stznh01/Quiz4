a3d312b13f13563941cf671493043ad432a7d6ca
Zixuan Huang <stznh01@moravian.edu>
2014-02-07 10:19:59 -0500

    Initial Commit

---
+++ Quiz4/Q4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Q4AppDelegate.h
+//  Quiz4
+//
+//  Created by Hansen on 2/7/14.
+//  Copyright (c) 2014 Zixuan Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Q4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz4
+//
+//  Created by Hansen on 2/7/14.
+//  Copyright (c) 2014 Zixuan Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Q4AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Q4AppDelegate class]));
+    }
+}
